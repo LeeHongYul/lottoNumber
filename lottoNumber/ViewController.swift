23a7aab9f3d3937873b11d388938c7426ffa3675
hongryul <ryullee0911@gmail.com>
2023-02-07 22:50:30 +0900

    Initial Commit

---
+++ lottoNumber/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lottoNumber
+//
+//  Created by 이홍렬 on 2023/02/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
